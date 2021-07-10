087300323b055e523ae44af94135145d8711952e
JTamarit <69979518+JTamarit@users.noreply.github.com>
2021-07-10 20:34:39 +0200

    Vamos progresando

--- scrapper/wapp7day.py
+++ scrapper/wapp7day.py
@@ -2,7 +2,7 @@ import requests
 
 api_key ="9a16a8e5458b6fdb0d040e46ee221bca"
 
-endpoint=(f"https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely&appid={api_key}")
+endpoint=(f"https://api.openweathermap.org/data/2.5/onecall?lat=39.3333&lon=-0.8333&units=metric&exclude=hourly,minutely&appid={api_key}")
 
 response= requests.get(endpoint)
 
