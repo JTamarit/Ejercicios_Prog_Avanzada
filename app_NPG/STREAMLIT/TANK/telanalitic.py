d1da5d09b56de46dde4d278775a66e3f22ed9a54
JTamarit <69979518+JTamarit@users.noreply.github.com>
2021-10-21 15:09:03 +0200

    ultimos cambio

---
+++ app_NPG/STREAMLIT/TANK/telanalitic.py
@@ -0,0 +1,11 @@
+import os
+import numpy
+import pandas
+
+
+#Transformar excell a pandas.
+# Obtener fecha inicio y fin
+# Tiempo transcurrido
+# Numero de descargas
+# Consumo en ese tiempo
+# Run outs?
--- app_NPG/STREAMLIT/TANK/teltank.py
+++ app_NPG/STREAMLIT/TANK/teltank.py
@@ -3,3 +3,15 @@ import numpy as np
 import pandas as pd
 import time
 import streamlit.components.v1 as components
+
+#Titulo app
+
+#Visualización datos del excel
+
+# Limites de Fechas
+
+# Numero descargas
+
+# Consumo
+
+# Graficos
