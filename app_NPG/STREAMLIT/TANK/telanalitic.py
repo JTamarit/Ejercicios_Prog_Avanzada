9def6bc4a6d8ef179cfe1446861fd0bf911eee97
JTamarit <69979518+JTamarit@users.noreply.github.com>
2021-10-28 21:51:02 +0200

    c

--- app_NPG/STREAMLIT/TANK/telanalitic.py
+++ app_NPG/STREAMLIT/TANK/telanalitic.py
@@ -3,7 +3,9 @@ import numpy
 import pandas
 
 
-#Transformar excell a pandas.
+#Transformar excel a dataframe.
+df=pd.read_excel('Telemetry.xlsx', index_col=0)
+
 # Obtener fecha inicio y fin
 # Tiempo transcurrido
 # Numero de descargas
