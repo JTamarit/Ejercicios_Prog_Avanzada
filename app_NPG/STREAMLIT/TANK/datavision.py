0a6284b5b6528f170c58fa7a2c9ddce58f2420ee
JTamarit <69979518+JTamarit@users.noreply.github.com>
2021-11-15 22:54:51 +0100

    Dataframe_consumo

--- app_NPG/STREAMLIT/TANK/datavision.py
+++ app_NPG/STREAMLIT/TANK/datavision.py
@@ -7,31 +7,98 @@ import matplotlib.colors
 from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
 
+class Model:
 
-def season_selector(date):
-    # Obtenemos la estación de año a partir de la fecha.
-    season=['Winter','April','Summer','Autum']
-    date=datetime.now()
-    day=int(date.strftime("%d"))
-    month=int(date.strftime("%m"))
-    if (month >= 1 and month < 3) or (month == 12 and day >= 21) or (month == 3 and day < 21) :
-        return season[0]
-    if (month > 3 and month < 6) or (month == 6 and day < 21) or (month == 3 and day < 21):
-        return season[1]
-    if (month > 6 and month < 9) or (month == 6 and day >= 21) or (month == 9 and day < 21):
-        return season[2]
-    if (month > 9 and month < 12) or (month == 9 and day >= 21) or (month == 12 and day < 21):
-        return season[3]
+    def season_selector(date):
+        # Obtenemos la estación de año a partir de la fecha.
+        season=['Winter','April','Summer','Autum']
+        date=datetime.now()
+        day=int(date.strftime("%d"))
+        month=int(date.strftime("%m"))
+        if (month >= 1 and month < 3) or (month == 12 and day >= 21) or (month == 3 and day < 21) :
+            return season[0]
+        if (month > 3 and month < 6) or (month == 6 and day < 21) or (month == 3 and day < 21):
+            return season[1]
+        if (month > 6 and month < 9) or (month == 6 and day >= 21) or (month == 9 and day < 21):
+            return season[2]
+        if (month > 9 and month < 12) or (month == 9 and day >= 21) or (month == 12 and day < 21):
+            return season[3]
 
-def inicio(nivel):
+    def inicio(nivel):
+        
+        num_valores=int(len(df['Nivel (%)']))
     
-    num_valores=int(len(df['Nivel (%)']))
-    
-    if nivel == df['Nivel (%)'].iloc[0]:
-        return "Inicio"
-    if nivel == df['Nivel (%)'].iloc[(num_valores-1)]:
-        return "Fin"
+        if nivel == df['Nivel (%)'].iloc[0]:
+            return "Inicio"
+        if nivel == df['Nivel (%)'].iloc[(num_valores-1)]:
+            return "Fin"
 
+    def descarga_consumo(df):
+        #Creamos una función que nos indica si el valor de nivel corresponde a un estado 
+        #de consumo o llenado de la cisterna:
+        #Creamos lista con todos los niveles:
+        niveles=[nivel for nivel in df['Nivel (%)']]
+
+        operacion=['inicio']
+        n=1
+        while n <(len(niveles)-1):
+            if niveles[n] > niveles[n+1] and niveles[n]< niveles[n-1]:
+                operacion.append("Consumo")
+    
+            elif niveles[n] < niveles[n+1] and niveles[n]> niveles[n-1]:
+                operacion.append("Descarga")
+        
+            elif niveles[n] < niveles[n+1] and niveles[n]< niveles[n-1]:
+                if (niveles[n+1]-niveles[n])>0.3 and (niveles[n-1]-niveles[n])>0.3:
+                    operacion.append("Empieza Descarga")
+                else:
+                    operacion.append("Consumo")
+                
+        
+            elif niveles[n] > niveles[n+1] and niveles[n]> niveles[n-1]:
+                if (niveles[n]-niveles[n+1])>0.3 and (niveles[n]-niveles[n-1])>0.3:
+                    operacion.append("Termina Descarga")
+                else:
+                    operacion.append("Consumo")
+        
+            elif niveles[n] == niveles[n+1]:
+        
+                operacion.append("Sin Consumo")
+        
+            elif niveles[n] == niveles[n-1]:
+        
+                operacion.append("Sin Consumo")
+            n+=1
+        
+        return operacion
+
+    def consumo(dff):
+
+        # Cálculo de consumo:
+
+        total_consumo=[]
+        parcial_consumo=[]
+
+        for i in range(len(dff)):
+    
+            if i==1 and dff.iloc[i,11]=='Consumo':
+                parcial_consumo.append(dff.iloc[0,0])
+                parcial_consumo.append(dff.iloc[0,3])
+                parcial_consumo.append(dff.iloc[0,11])
+            
+            if i > 1 and dff.iloc[i,11] == 'Consumo' and dff.iloc[(i-1),11] == 'Termina Descarga':
+                parcial_consumo.append(dff.iloc[(i-1),0])
+                parcial_consumo.append(dff.iloc[(i-1),3])
+                parcial_consumo.append(dff.iloc[(i-1),11])
+    
+            if i > 1 and dff.iloc[i,11]=='Empieza Descarga':
+                parcial_consumo.append(dff.iloc[(i),0])
+                parcial_consumo.append(dff.iloc[(i),3])
+                parcial_consumo.append(dff.iloc[(i),11])
+                total_consumo.append(parcial_consumo)
+                parcial_consumo =[]
+
+        return total_consumo
 
 class Telemetry:
 
@@ -105,16 +172,35 @@ class Analisys:
         self.df['Date'] = self.df['Date'].dt.strftime('%d-%m-%Y')
 
         # Clasificamos por epoca del año:
-        self.df['Season'] = self.df['Date'].apply(season_selector)
+        self.df['Season'] = self.df['Date'].apply(Model.season_selector)
 
         #Eliminamos valor 0 de la columna Nivel:
         self.df=self.df.drop(self.df.loc[df['Nivel (%)']== 0].index)
 
         # Creamos un columna en el Dataframe llamada Estado:
-        self.df['Estado']=self.df['Nivel (%)'].apply(inicio)
+        self.df['Estado']=self.df['Nivel (%)'].apply(Model.inicio)
+
+        # Clasificamos estado:
+
+        estado= Model.descarga_consumo(self.df)
+
+        # Hacemos una copia del Dataframe y llenamos el campo Estado con su valor:
+        dff=self.df.copy()
+        mask=dff['Estado']
+        for n in range(len(estado)):
+            mask.iloc[n]=estado[n]
+        
+        self.df=dff
 
         return self.df
 
+    def consumo_total(self,df,tank_size):
+        # Creamos el Dataframe Consumo_df para trabajar con los datos de consumo:
+        consumo_df=pd.DataFrame(Model.consumo(df))
+        consumo_df.columns=['Timestamp_0','Nivel_0','Estado_0','Timestamp_f', 'Nivel_f', 'Estado_f']
+        consumo_df['Delta_Time']=consumo_df['Timestamp_f']-consumo_df['Timestamp_0']
+        consumo_df['Consumo m3']=(consumo_df['Nivel_0']-consumo_df['Nivel_f'])*int(tank_size)/100
+        return consumo_df
 
 
 
@@ -125,6 +211,9 @@ tnk=Tank(info)
 #tank_v=tnk.tank_volum()
 #nm_tank=tnk.number_SSTT()
 #print(tank_v, nm_tank)
+tank_size=tnk.tank_volum()
+print(tank_size)
 anl=Analisys(df)
 df=anl.build()
-print(df)
+consumo_df=anl.consumo_total(df,tank_size)
+print(consumo_df)
